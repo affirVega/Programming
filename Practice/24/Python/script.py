479a37cba52f2d61e4cc543a6455757ec4ef72ad
Affir Vega <affirvega@krutt.org>
2020-11-16 12:05:09 +0300

    Small changes

--- Practice/24/Python/script.py
+++ Practice/24/Python/script.py
@@ -9,22 +9,23 @@ for task in j:
     userId    = task['userId']
     completed = task['completed']
 
+    # Find user in out with userId
     selectedUser = None
     for user in out:
         if user['userId'] == userId:
             selectedUser = user
             break
 
+    # If this user was not found, create one
     if selectedUser == None:
         selectedUser = {}
         selectedUser['userId'] = userId
         selectedUser['task_completed'] = 0
         out.append(selectedUser)
 
+    # If task is completed, add one task to completed counter
     if completed:
         selectedUser['task_completed'] += 1
-
-print(json.dumps(out, indent=2))
 
 with open('out.json', 'w') as f:
     f.write(json.dumps(out, indent=2))
