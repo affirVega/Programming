be4a0539a3e53429ab8f0303d3399e18462a381c
Affir Vega <affirvega@krutt.org>
2020-11-17 00:56:18 +0300

    Redesigning solution with maps

--- Practice/24/C++/main.cpp
+++ Practice/24/C++/main.cpp
@@ -1,60 +1,37 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <map>
 
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
-json* find_by_user_id(json &container, int userId) {
-	json temp;
-	json *user = &temp;
-
-	std::string field = "userId";
-
-	for (auto& element : container) {
-		if (element[field] == userId) {
-			user = &element;
-			break;
-		}
-	}
-
-	return user;
-}
-
-json* create_if_empty(json &container, json *object, int userId) {
-	if (object->empty()) {
-		container.push_back({
-			{"userId", userId},
-			{"task_completed", 0}
-		});
-		
-		// make `object` point to created object in container
-		object = &*--container.end();
-	}
-
-	return object;
-}
-
 int main() {
-	std::ifstream input("in.json");
-	std::ofstream output("out.json");
-	
-	json jinput;
-	input >> jinput;
+    std::ifstream input("in.json");
+    json jinput; input >> jinput; input.close();
 
-	json joutput;
-	for (auto& element : jinput) {
-		int  userId    = element["userId"];
-		bool completed = element["completed"];
+	std::map<int, int> users;
+    for (auto& task : jinput) {
+        int  userId    = task["userId"];
+        bool completed = task["completed"];
 
-		json &user = *create_if_empty(joutput,
-									  find_by_user_id(joutput, userId),
-									  userId);
-
+		users.try_emplace(userId, 0);
 		if (completed) {
-			user["task_completed"] = static_cast<int>(user["task_completed"]) + 1;
+			++users[userId];
 		}
+    }
+
+    json joutput;
+	for (auto& pair : users) {
+		joutput.push_back(
+			{
+				{"userId", pair.first},
+				{"task_completed", pair.second}
+			}
+		);
 	}
 
-	output << std::setw(2) << joutput << std::endl;
+    std::ofstream output("out.json");
+    output << std::setw(2) << joutput << std::endl;
+	output.close();
 }
