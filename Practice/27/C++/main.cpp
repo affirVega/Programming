0e12b4a3a4d123ef1c386d2acecaa6ad0bd39284
Feskow Vega <affirvega@krutt.org>
2020-12-09 18:40:06 +0300

    27

--- Practice/27/C++/main.cpp
+++ Practice/27/C++/main.cpp
@@ -1,80 +1,42 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-template <class T>
-struct linklist_item
-{
-    T val;
-    linklist_item *next = nullptr;
-    
-    /*
-    void push_back(linklist_item &item)
-    {
-        linklist_item *temp = *this;
-        while (temp->next != nullptr)
-        {
-            temp = temp->next;
-        }
-        temp->next = &item;
-    }*/
-    
-    void insert(linklist_item &item)
-    {
-        item.next = this.next;
-        this.next = &item;
-    }
-};
+#define if if(
+#define for for(
+#define print(A) cout<<A<<flush
+#define println(A) cout<<A<<endl
+#define input(N) (print(N),cin>>temp,temp)
+#define vec vector<int64_t> vec
 
+// Как же я люблю программировать на Python++
 
-int main()
-{
-    int64_t n;
-    std::cout << "Введите число n: " << std::flush;
-    std::cin >> n;
+int main()                                                                      {
+    int64_t temp                                                                ;
+    int64_t n = input("Введите число n: ")                                      ;
+    int64_t value = input("Введите n чисел, разделённые пробелом:\n> ")         ;
     
-    int value;
-    std::cout << "Введите n чисел, разделённые пробелом:\n> " << std::flush;
-    std::cin >> value;
-    linklist_item<int64_t> head = {value};
-    
-    for (int64_t i = 1; i < n; ++i)
-    {
-        std::cin >> value;
-        linklist_item<int64_t> *item;
-        for (int j = 0, item = &head;
-            j < 5;
-            ++j)
-        {
-            if (value > item->value)
-            {
-                linklist_item<int64_t> temp = {value};
-                temp.insert(*item);
-                if (item == &head)
-                {
-                    head = temp;
-                }
-                break;
-            }
-            else if (item->next == nullptr)
-            {
-                break;
-            }
-            else
-            {
-                item = item->next;
-            }
-        }
+    vec = {value}                                                               ;
+    println(value)                                                              ;
+                                                                                #undef vec
+    for int64_t i = 1; i < n; ++i                                               ){
+        cin >> value                                                            ;
+        int j                                                                   ;
         
-        for (int j = 0, item = &head; j < 5; ++j)
-        {
-            if (item->next != nullptr)
-            {
-                std::cout << item->value << ' ';
-            }
-            else
-            {
-                break;
-            }
-        }
-        std::cout << std::endl;
-    }
-}
+        for j = 0; j < 5 and j < vec.size(); ++j                                ){
+            if value > vec[j]                                                   ){
+                vec.insert(vec.begin() + j, value)                              ;
+                break                                                           ;}}
+                
+        if j == 5 or j == vec.size()                                            ){
+            vec.push_back(value)                                                ;}
+            
+        if vec.size() > 5                                                       ){
+            vec.erase(vec.begin())                                              ;}
+            
+        for int i = 0; i < vec.size(); ++i                                      ){
+            cout << vec[i] << ' '                                               ;}
+            
+        cout << endl                                                            ;}}
+
+
