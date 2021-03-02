43073a3b688afb0ea46cfb50270c079b2895e407
Feskow Vega <affirvega@krutt.org>
2021-03-02 17:37:11 +0300

    35

---
+++ Practice/35/C++/code.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include "IntArray.h"
+
+int main(int argc, char *argv[])
+{
+    int size = 30;
+    IntArray arr;
+
+    create(arr, size);
+    for (int i = 0; i < size; ++i)
+        set(arr, i, i+1);
+    print(arr);
+
+    resize(arr, 50);
+    print(arr);
+
+    resize(arr, 10);
+    destroy(arr);
+}
+
